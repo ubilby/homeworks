1f2862387e84e9c9a93ec614ad0db79f797f61d7
ubilby <ubilby@mail.ru>
2017-06-19 16:12:37 +0300

    подредактировал методы в дочерних классах

--- 190617/main.py
+++ 190617/main.py
@@ -92,35 +92,35 @@ class XmlParamHandler(ParamHandler):
 
 
 class JsonParamHandler(ParamHandler):
-    def read(self, name):
+    def read(self):
         """
         Чтение в формате JSON и присвоение значений в self.params
         """
-        with open(name) as f: #json любит словари, все ключи - обязательно в кавычках, нет None, но есть Null
+        with open(self.source) as f: #json любит словари, все ключи - обязательно в кавычках, нет None, но есть Null
             print("\nДанные прочитаны из json файла: {}\n".format(json.load(f)))
 
 
-    def write(self, name):
+    def write(self):
         """
         Запись в формате JSON параметров self.params
         """
-        with open(name, "w") as f:      #только в режиме перезаписи
+        with open(self.source, "w") as f:      #только в режиме перезаписи
             json.dump(self.params, f)
 
 
 class PickleParamHandler(ParamHandler):
-    def read(self, name):
+    def read(self):
         """
         Чтение в формате pickle и присвоение значений в self.params
         """
-        with open(name, "rb") as f:
+        with open(self.source, "rb") as f:
             print("\nДанные прочитаны из pickle файла: {}\n".format(pickle.load(f)))
 
-    def write(self, name):
+    def write(self):
         """
         Запись в формате pickle параметров self.params
         """
-        with open(name, "wb") as f:
+        with open(self.source, "wb") as f:
             pickle.dump(self.params, f)
 
 
@@ -133,15 +133,15 @@ config = ParamHandler.get_instance('./params.json')
 config.add_param('key1', 'val1')
 config.add_param('key2', 'val2')
 config.add_param('key3', 'val3')
-config.write("params.json")
-config.read("params.json")
+config.write()
+config.read()
 
 config = ParamHandler.get_instance('./params.pickle')
 config.add_param('key1', 'val1')
 config.add_param('key2', 'val2')
 config.add_param('key3', 'val3')
-config.write("params.pickle")
-config.read("params.pickle")
+config.write()
+config.read()
 
 
 
